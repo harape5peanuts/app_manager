4cec84fb14cfb00a9d68325f631f0a316879723b
harepe5peanuts <harapeko.pe5pe5@gmail.com>
2020-05-25 18:50:44 +0900

    🍎 update: アプリを画面に３列で表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:typed_data';
+
+import 'package:device_apps/device_apps.dart';
+import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,16 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lime,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -28,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,68 +31,120 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int currentPage = 0;
+
+  GlobalKey bottomNavigationKey = GlobalKey();
   int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        title: Text(
+          widget.title,
+          style: GoogleFonts.kanit(
+            textStyle: TextStyle(
+              decoration: TextDecoration.none,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: FutureBuilder(
+        // `device_apps` パッケージを利用して端末にインストールされているアプリの一覧を取得している
+        future: DeviceApps.getInstalledApplications(
+          includeAppIcons: true,
+          includeSystemApps: true,
+          onlyAppsWithLaunchIntent: true,
+        ),
+        builder: (context, data) {
+          // 非同期処理中の判断
+          if (data.data == null) {
+            // データ取得前はローディング中のプログレスを表示
+            return Center(
+              child: const CircularProgressIndicator(),
+            );
+          } else {
+            // データ取得後はリストビューに情報をセット
+            final apps = data.data as List<Application>;
+
+            return GridView.builder(
+              itemCount: apps.length,
+              primary: false,
+              padding: const EdgeInsets.all(40),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 50,
+                mainAxisSpacing: 40,
+                crossAxisCount: 3,
+              ),
+              itemBuilder: (context, index) {
+                final app = apps[index];
+                return Container(
+                  child: app is ApplicationWithIcon
+                  // アイコンを持っているアプリ（ ApplicationWithIcon インスタンス）の場合はアイコンを表示する
+                      ? CircleAvatar(
+                    backgroundImage: MemoryImage(app.icon as Uint8List),
+                    backgroundColor: Colors.white,
+                  )
+                  // ない場合はアイコンなし
+                      : null,
+                );
+              },
+            );
+          }
+        },
+      ),
+      bottomNavigationBar: FancyBottomNavigation(
+        tabs: [
+          TabData(
+              iconData: Icons.home,
+              title: "Home",
+              onclick: () {
+                print('home');
+              }),
+          TabData(
+              iconData: Icons.search,
+              title: "Search",
+              onclick: () {
+                print('Search');
+              }),
+          TabData(iconData: Icons.shopping_cart, title: "Basket")
+        ],
+        initialSelection: 1,
+        key: bottomNavigationKey,
+        onTabChangedListener: (position) {
+          setState(() {
+            currentPage = position;
+          });
+        },
+      ),
     );
   }
+}
+
+class _AppInfo extends StatelessWidget{
+  final String title;
+  final Color color;
+
+  const _AppInfo({Key key, this.title, this.color}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return InkWell(
+      onTap: () {
+        print("aaa");
+      },
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        child: Text(title),
+        color: color,
+      ),
+    );
+  }
+
 }
