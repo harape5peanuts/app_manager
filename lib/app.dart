6fecb7cc63b2b6d73e2949f13b02323f38f85783
harepe5peanuts <harapeko.pe5pe5@gmail.com>
2020-07-31 20:01:35 +0900

    ✨ update: フォント直した！！

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'main.dart';
 
@@ -12,8 +13,22 @@ class App extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.lime,
+        textTheme: TextTheme(
+          headline1: GoogleFonts.mPLUS1p(fontSize: 96),
+          headline2: GoogleFonts.mPLUS1p(fontSize: 60),
+          headline3: GoogleFonts.mPLUS1p(fontSize: 48),
+          headline4: GoogleFonts.mPLUS1p(fontSize: 34),
+          headline5: GoogleFonts.mPLUS1p(fontSize: 24),
+          headline6: GoogleFonts.mPLUS1p(fontSize: 20),
+          subtitle1: GoogleFonts.mPLUS1p(fontSize: 16),
+          subtitle2: GoogleFonts.mPLUS1p(fontSize: 14),
+          bodyText1: GoogleFonts.mPLUS1p(fontSize: 16),
+          bodyText2: GoogleFonts.mPLUS1p(fontSize: 14),
+        ),
       ),
+
       home: Main(title: 'APP MANAGER'),
+
     );
   }
 }
